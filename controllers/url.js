49ec06fafc53749e161164423ea9378a9b326281
Mahesh <yanagundim@gmail.com>
2026-01-15 15:04:30 +0530

    generating same shortId for same original url

--- controllers/url.js
+++ controllers/url.js
@@ -5,6 +5,15 @@ require('dotenv').config();
 async function handleGenerateNewShortURL(req,res){
     const body=req.body;
     if(!body.url) return res.status(400).json({error: 'url is required'});
+    const existing = await URL.findOne({ redirectURL: body.url });
+    if (existing) {
+    const allurls = await URL.find({});
+    return res.render("home", {
+        id: existing.shortId,
+        urls: allurls,
+        APP_URL: process.env.APP_URL
+    });
+  }
     const shortID=nanoid(8);
     await URL.create({
         shortId:shortID,
